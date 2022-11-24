6c2a25e4deacaf62a128b1feb16a3159cf890b29
Evan Relf <evanr@mercury.com>
2022-11-23 16:45:57 -0800

    Create `users` table

--- src/Slacklinker/Models.hs
+++ src/Slacklinker/Models.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Slacklinker.Models where
 
 import Data.Aeson (Value (..))
+import Database.Persist (PersistField)
+import Database.Persist.Postgresql (PersistFieldSql)
 import Slacklinker.PersistImport
 import Slacklinker.Settings.Types
 import Slacklinker.Types
+import Web.Slack.Types qualified as Slack
+
+deriving newtype instance PersistField Slack.UserId
+deriving newtype instance PersistFieldSql Slack.UserId
 
 $(mkModelUnprefixed $(discoverEntities) $(modelFile "models"))
