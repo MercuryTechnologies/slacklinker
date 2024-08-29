82ae2fa4822bb2cf0289175c89ac769fd928feb3
Lev Dubinets <3114081+ldub@users.noreply.github.com>
2024-08-28 18:23:23 -0700

    fix typo

--- default.nix
+++ default.nix
@@ -2,8 +2,7 @@
   (
     let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
     fetchTarball {
-      url = "https://github.com/mercurytechnologies/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz"
-      url = lock.nodes.flake-compat.locked.url;
+      url = "https://github.com/mercurytechnologies/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
       sha256 = lock.nodes.flake-compat.locked.narHash;
     }
   )
