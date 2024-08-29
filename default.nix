6a8440783b3a5bc048028c7a7c33dbb7192dcd70
Lev Dubinets <3114081+ldub@users.noreply.github.com>
2024-08-28 18:19:25 -0700

    switch to temporary flake-compat mirror

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
   (
     let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
     fetchTarball {
+      url = "https://github.com/mercurytechnologies/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz"
       url = lock.nodes.flake-compat.locked.url;
       sha256 = lock.nodes.flake-compat.locked.narHash;
     }
