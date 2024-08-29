1435b87b10185a6992b9cf65b5fbf20a7df24d14
Lev Dubinets <3114081+ldub@users.noreply.github.com>
2024-08-28 18:21:17 -0700

    flake-compat mirror (#28)
    * switch to temporary flake-compat mirror
    
    * switch to temporary flake-compat mirror

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
-      url = "https://git.lix.systems/lix-project/flake-compat/archive/main.tar.gz";
+      url = "github:mercurytechnologies/flake-compat";
       flake = false;
     };
   };
