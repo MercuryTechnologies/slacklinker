99040be997e2cdd179bf3f7b570f36bfe77b628a
Lev Dubinets <3114081+ldub@users.noreply.github.com>
2024-09-05 11:18:47 -0700

    monad fail and fix test

--- test/Slacklinker/Handler/WebhookSpec.hs
+++ test/Slacklinker/Handler/WebhookSpec.hs
@@ -81,6 +81,9 @@ botMessageEventWithBlocks ts blocks =
 updateThreadTs :: MessageEvent -> Maybe Text -> MessageEvent
 updateThreadTs MessageEvent {..} newThreadTs = MessageEvent {threadTs = newThreadTs, ..}
 
+updateChannelId :: MessageEvent -> ConversationId -> MessageEvent
+updateChannelId MessageEvent {..} newChannelId = MessageEvent {channel = newChannelId, ..}
+
 doLink :: (HasApp m, MonadUnliftIO m) => TeamId -> Text -> Text -> m MessageEvent
 doLink teamId ts url = do
   let msg = messageEventWithBlocks ts [SlackBlockRichText . urlRichText $ url]
@@ -101,7 +104,7 @@ spec = do
         (wsId, teamId) <- createWorkspace
         let (url, _parts) = sampleUrl
         msg <- doLink teamId ts1 url
-        ~(Just (Entity _ userData)) <- runDB . getBy $ UniqueKnownUser wsId msg.user
+        (Just (Entity _ userData)) <- runDB . getBy $ UniqueKnownUser wsId msg.user
         liftIO $ userData.emoji `shouldBe` Nothing
 
   withApp $ describe "Should insert RepliedThread for a message" do
@@ -111,11 +114,10 @@ spec = do
         let (url, parts) = sampleUrl
         msg <- doLink teamId ts1 url
 
-        -- FIXME: MonadFail instead of irrefutable pattern crimes
-        ~(Just (Entity rtId _thread)) <- runDB $ getBy $ UniqueRepliedThread wsId parts.channelId parts.messageTs
+        Just (Entity rtId _thread) <- runDB $ getBy $ UniqueRepliedThread wsId parts.channelId parts.messageTs
 
-        ~[Entity _ theLink] <- runDB $ selectList [LinkedMessageRepliedThreadId ==. rtId] []
-        ~(Just (Entity channelId _)) <- runDB $ getBy $ UniqueJoinedChannel wsId msg.channel
+        [Entity _ theLink] <- runDB $ selectList [LinkedMessageRepliedThreadId ==. rtId] []
+        (Just (Entity channelId _)) <- runDB $ getBy $ UniqueJoinedChannel wsId msg.channel
 
         liftIO $ do
           -- This should name the message that triggered slacklinker
@@ -129,11 +131,10 @@ spec = do
         let (url, parts) = sampleUrl
         msg <- doBotLink teamId ts1 url
 
-        -- FIXME: MonadFail instead of irrefutable pattern crimes
-        ~(Just (Entity rtId _thread)) <- runDB $ getBy $ UniqueRepliedThread wsId parts.channelId parts.messageTs
+        (Just (Entity rtId _thread)) <- runDB $ getBy $ UniqueRepliedThread wsId parts.channelId parts.messageTs
 
-        ~[Entity _ theLink] <- runDB $ selectList [LinkedMessageRepliedThreadId ==. rtId] []
-        ~(Just (Entity channelId _)) <- runDB $ getBy $ UniqueJoinedChannel wsId msg.channel
+        [Entity _ theLink] <- runDB $ selectList [LinkedMessageRepliedThreadId ==. rtId] []
+        (Just (Entity channelId _)) <- runDB $ getBy $ UniqueJoinedChannel wsId msg.channel
 
         liftIO $ do
           -- This should name the message that triggered slacklinker
@@ -143,18 +144,26 @@ spec = do
           theLink.sent `shouldBe` False
 
     it "will not link a message within the same thread" \app -> do
+      -- the setup should be
+      -- lev: heres a parent message 
+      --  |-> (in thread) lev: i am linking to https://myworkspace.slack.com/archives/C045V0VJT16/p1725559477299859
+      --
+      -- where the parent message url is: https://myworkspace.slack.com/archives/C045V0VJT16/p1725559477299859
+      -- and the thread message url is https://myworkspace.slack.com/archives/C045V0VJT16/p1725559485267619?thread_ts=1725559477.299859
+      --
+      -- importantly, parent.message_ts == child.thread_ts
+      -- and parent.channel_id == child.channel_id
       runAppM app $ do
         (wsId, teamId) <- createWorkspace
-        let (url, parts) = sampleUrl
-        let msg =
-              updateThreadTs
-                (messageEventWithBlocks ts1 [SlackBlockRichText . urlRichText $ url])
-                (Just parts.messageTs)
+        let (parentUrl, parentParts) = sampleUrl
+            childMessage = messageEventWithBlocks ts1 [SlackBlockRichText . urlRichText $ parentUrl]
+            childMessageWithThread = updateThreadTs childMessage (Just parentParts.messageTs)
+            childMessageWithChannel = updateChannelId childMessageWithThread parentParts.channelId
 
-        handleMessage msg teamId
+        handleMessage childMessageWithChannel teamId
 
         -- We should not plan a reply to a thread that links to itself
-        ~Nothing <- runDB $ getBy $ UniqueRepliedThread wsId parts.channelId parts.messageTs
+        Nothing <- runDB $ getBy $ UniqueRepliedThread wsId parentParts.channelId parentParts.messageTs
         pure ()
 
     it "will file a link to a message downthread as the same thread as linking the parent" \app -> do
@@ -171,7 +180,7 @@ spec = do
         -- Verify the test data reproduces the expected condition
         liftIO $ childUrlParts.threadTs `shouldBe` Just parentUrlParts.messageTs
 
-        ~(Just _) <-
+        (Just _) <-
           runDB $
             getBy $
               UniqueRepliedThread
